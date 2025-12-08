dc8958916b04741a0cdd3bae15ea291938f1a938
Ever Godoy <everman77@gmail.com>
2025-12-08 16:28:16 -0300

    Reto 1 - Filtrar los regalos defectuosos

---
+++ README.md
@@ -0,0 +1,3 @@
+## Soluciónes AdventJS 2025
+
+En este repositorio subiré mis soluciónes a los retos del AdventJS 2025 creado por MuduDEV - https://adventjs.dev/es
---
+++ Reto-1/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} gifts - The array of gifts to filter
+ * @returns {string[]} An array with the unique filtered gifts
+ */
+function filterGifts(gifts) {
+  const filteredGifts = gifts.filter(gift => {
+    if (!gift.includes('#')) {
+      return gift
+    }
+  })
+  return filteredGifts
+}
+
+// Ejemplos
+const gifts1 = ['car', 'doll#arm', 'ball', '#train']
+const good1 = filterGifts(gifts1)
+console.log(good1) // ['car', 'ball']
+
+const gifts2 = ['#broken', '#rusty']
+const good2 = filterGifts(gifts2)
+console.log(good2) // []
+
+const gifts3 = []
+const good3 = filterGifts(gifts3)
+console.log(good3) // []
